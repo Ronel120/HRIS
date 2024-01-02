015bfc231ece31e934c24aadb473c1a86869c550
ronel <ronellinsangan1@gmail.com>
2024-01-02 18:13:51 +0800

    Refactor the create, add, update methods

--- src/main/java/com/example/employee/controller/EmployeeController.java
+++ src/main/java/com/example/employee/controller/EmployeeController.java
@@ -1,35 +1,41 @@
 package com.example.employee.controller;
 
 import com.example.employee.entity.Employee;
-import com.example.employee.repository.EmployeeRepository;
-import com.example.employee.util.EmployeeNumberGenerator;
+import com.example.employee.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/employees")
 public class EmployeeController {
 
-    private final EmployeeRepository employeeRepository;
-
-    private final EmployeeNumberGenerator generator;
+    private final EmployeeService employeeRepository;
 
     @Autowired
-    EmployeeController(EmployeeRepository repository, EmployeeNumberGenerator generator1) {
-        employeeRepository = repository;
-        generator = generator1;
+    EmployeeController(EmployeeService employeeRepository) {
+        this.employeeRepository = employeeRepository;
     }
 
     @PostMapping
     public ResponseEntity<Employee> add(@RequestBody Employee employee) {
-        String employeeNumber = generator.generate();
-        employee.setEmployeeNumber(employeeNumber);
-        return new ResponseEntity<>(employeeRepository.save(employee), HttpStatus.OK);
+        return new ResponseEntity<>(employeeRepository.add(employee), HttpStatus.OK);
     }
 
+    @GetMapping
+    public ResponseEntity<List<Employee>> list() {
+        return new ResponseEntity<>(employeeRepository.list(), HttpStatus.OK);
+    }
 
+    @PutMapping("/{id}")
+    public ResponseEntity<Employee> update(@PathVariable("id") Long id,
+                                           @RequestBody Employee employee) {
+        return new ResponseEntity<>(employeeRepository.update(id, employee), HttpStatus.OK);
+
+    }
 
 
 }
